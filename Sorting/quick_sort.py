ad4ccae29098227313837dc2ab2374d87a96d219
Jun0414 <gbwj123@naver.com>
2021-02-13 16:51:59 +0900

    Update quick_sort.py

--- Sorting/quick_sort.py
+++ Sorting/quick_sort.py
@@ -38,3 +38,31 @@ print(arr)
 
 # 출력 예시
 # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+# 파이썬 전용 퀵정렬(속도는 조금 느리다)
+
+arr = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort_py(array):
+    # 리스트가 하나 이하의 원소만 담고있으면 종료
+    if len(array) <= 1:
+        return array
+
+    pivot = array[0]
+    # 피벗을 제외한 리스트
+    tail = array[1:]
+
+    # 분할된 왼쪽 부분
+    left_side = [x for x in tail if x <= pivot]
+    # 분할된 오른쪽 부분
+    right_side = [y for y in tail if y > pivot]
+
+    # 분할 이후 왼쪽과 오른쪽 부분에서 각각 정렬 수행하고, 전체 리스트 반환
+    return quick_sort_py(left_side) + [pivot] + quick_sort_py(right_side)
+
+print(quick_sort_py(arr))
+
+# 출력 예시
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
